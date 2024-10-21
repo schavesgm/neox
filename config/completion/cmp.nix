43e87fb5888849c76f97e88d04b976e422981866
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2024-10-21 16:07:03 +0100

    fix(completion): 🐛 simplify `<Tab>` completion keymap

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -56,19 +56,12 @@
                 if cmp.visible() then
                   cmp.select_next_item()
                 elseif require("luasnip").expand_or_jumpable() then
-                  require("luasnip").expand_or_jump()
-                else
-                  fallback()
-                end
-              end, { "i", "s" })
-            '';
-
-            "<S-Tab>" = ''
-              cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_prev_item()
-                elseif require("luasnip").locally_jumpable(-1) then
-                  require("luasnip").jump(-1)
+                  vim.fn.feedkeys(
+                    vim.api.nvim_replace_termcodes(
+                      "<Plug>luasnip-expand-or-jump", true, true, true
+                      ),
+                      ""
+                  )
                 else
                   fallback()
                 end
