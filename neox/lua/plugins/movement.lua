759deb47dec0ff6df04fee49325469984765b89d
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2024-12-20 18:01:40 +0000

    feat(plugins): ✨ modify `harpoon` keybindings

--- neox/lua/plugins/movement.lua
+++ neox/lua/plugins/movement.lua
@@ -6,10 +6,10 @@ local harpoon = require("harpoon")
 harpoon:setup()
 
 _G.neox.set_keymap("n", "<leader>a", function() harpoon:list():add() end)
-_G.neox.set_keymap("n", "<C-h>", function() harpoon:list():select(1) end)
-_G.neox.set_keymap("n", "<C-t>", function() harpoon:list():select(2) end)
-_G.neox.set_keymap("n", "<C-n>", function() harpoon:list():select(3) end)
-_G.neox.set_keymap("n", "<C-s>", function() harpoon:list():select(4) end)
+_G.neox.set_keymap("n", "<C-1>", function() harpoon:list():select(1) end)
+_G.neox.set_keymap("n", "<C-2>", function() harpoon:list():select(2) end)
+_G.neox.set_keymap("n", "<C-3>", function() harpoon:list():select(3) end)
+_G.neox.set_keymap("n", "<C-4>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 _G.neox.set_keymap("n", "<C-p>", function() harpoon:list():prev() end)
