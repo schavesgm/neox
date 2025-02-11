38ebf62a84176473576e700d4a6cd6378e3f7656
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2025-02-11 17:20:55 +0000

    fix(plugins): 🐛 avoid collisions between keymaps

--- neox/lua/plugins/picker.lua
+++ neox/lua/plugins/picker.lua
@@ -3,6 +3,6 @@
 --- Set some telescope keybindings in the system
 _G.neox.set_keymap("n", "<leader>ta", "<cmd>FzfLua<CR>")
 _G.neox.set_keymap("n", "<leader>tf", "<cmd>FzfLua files<CR>")
-_G.neox.set_keymap("n", "<leader>tb", "<cmd>FzfLua buffers<CR>")
+_G.neox.set_keymap("n", "<leader>th", "<cmd>FzfLua buffers<CR>")
 _G.neox.set_keymap("n", "<leader>tg", "<cmd>FzfLua live_grep<CR>")
 _G.neox.set_keymap("n", "<leader>tt", "<cmd>FzfLua resume<CR>")
