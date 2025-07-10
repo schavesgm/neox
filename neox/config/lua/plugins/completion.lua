784323bb1bce3b9c1569f5d33718f8350b253cdf
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2025-07-10 15:45:40 +0200

    feat(plugins): ✨ minor modifications to `luasnip` integration

--- neox/config/lua/plugins/completion.lua
+++ neox/config/lua/plugins/completion.lua
@@ -2,12 +2,14 @@
 
 -- Load the snippets module
 local luasnip = require("luasnip")
-luasnip.config.set_config({
-	history = true,
-	updateevents = "TextChanged,TextChangedI",
-	region_check_events = "InsertEnter",
-	delete_check_events = "InsertLeave",
-})
+
+-- TODO(@sergio.chaves): Investigate if this is necessary and decommission if not
+-- luasnip.config.set_config({
+--     history = true,
+--     updateevents = "TextChanged,TextChangedI",
+--     region_check_events = "InsertEnter",
+--     delete_check_events = "InsertLeave",
+-- })
 
 -- Load some pre-defined snippets
 require("luasnip.loaders.from_vscode").lazy_load()
@@ -17,20 +19,20 @@ luasnip.filetype_extend("NeogitCommitMessage", { "gitcommit" })
 luasnip.filetype_extend("octo", { "markdown" })
 
 require("blink.cmp").setup({
-	completion = {
-		keyword = { range = "full" },
-		documentation = {
-			auto_show = true,
-			auto_show_delay_ms = 500,
-		},
-	},
-	keymap = {
-		preset = "default",
-	},
-	snippets = {
-		preset = "luasnip",
-	},
-	signature = {
-		enabled = true,
-	},
+    completion = {
+        keyword = { range = "prefix" },
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 500,
+        },
+    },
+    keymap = {
+        preset = "default",
+    },
+    snippets = {
+        preset = "luasnip",
+    },
+    signature = {
+        enabled = true,
+    },
 })
